cc18aae25b9e8755905039c81f516880bdf200e8
dgeel <eldgeel1000@live.com>
2021-03-15 19:11:32 +0300

    bubleSort

--- bubleSort.py
+++ bubleSort.py
@@ -1,4 +1,8 @@
 
+# Note: in buble sort we compare the first element array with the second element 
+# if first element is greater we swap first with second
+# in each loop we decrement the value of i and j loop in range from 0 to value of current i 
+
 def bubleSort(nums):
     for i in range(len(nums)-1,0,-1):
         for j in range(i):
@@ -12,3 +16,4 @@ def bubleSort(nums):
 nums = [7,5,2,-1,6,9]
 bubleSort(nums)
 print(nums)
+
