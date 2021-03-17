acb01d6e42c95475eeac5490fbc212681e1f2a68
dgeel <eldgeel1000@live.com>
2021-03-17 22:31:25 +0300

    bubleSort.cpp

--- bubleSort.py
+++ bubleSort.py
@@ -6,16 +6,27 @@
 #in each loop we decrement the value of i and j loop in range from 0 to value of current i
  
 
-def bubleSort(nums):
-    for i in range(len(nums)-1,0,-1):
-        for j in range(i):
-            if nums[j] > nums[j+1]:
-                temp =  nums[j]
-                nums[j] = nums[j+1]
-                nums[j+1] = temp
-
-
-nums = [7,5,2,-1,6,9]
-bubleSort(nums)
-print(nums)
-
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n-1): 
+    # range(n) also work but outer loop will repeat one time more than needed. 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+  
+# Driver code to test above 
+arr = [64, 34, 25, 12, 22, 11, 90] 
+  
+bubbleSort(arr) 
+  
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print ("%d" %arr[i]),
